ac498c6858e8491e2c67815d8c4be1839752a48d
FlavioOtt <ottflavio1@gmail.com>
2024-08-12 17:27:26 -0300

    pokedex static | 12/08/2024 17:27

--- src/Components/Pokedex.jsx
+++ src/Components/Pokedex.jsx
@@ -1,10 +1,43 @@
-export default function Pokemons() {
+import "./styles/Pokedex.css";
 
+
+export default function Pokemons() {
 
     return (
         <div className="Pokemons">
             
             <h1>POKEMONS</h1>
+            <div className="pokedex-item pokedex-flex-column">
+                <div className="pokedex-item-div-top pokedex-flex-column">
+                    <div className="pokedex-item-div-img">
+                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png" alt="" />
+                    </div>
+
+                    <div className="pokedex-flex-column">
+                        <h3 className="pokedex-item-title">Charmander</h3>
+                        <p className="pokedex-item-level">Lv. XXX</p>
+                    </div>
+                </div>
+
+                <div className="pokedex-item-div-bottom pokedex-flex-column">
+                    <div className="pokedex-item-div-stats">
+                        <p>Tipo</p>
+                        <p>tipolongomsm</p>
+                    </div>
+                    <div className="pokedex-item-div-stats">
+                        <p>Vida</p>
+                        <p>100</p>
+                    </div>
+                    <div className="pokedex-item-div-stats">
+                        <p>Ataq. Base</p>
+                        <p>100</p>
+                    </div>
+                    <div className="pokedex-item-div-stats">
+                        <p>Def. Base</p>
+                        <p>100</p>
+                    </div>
+                </div>
+            </div>
     
         </div>
     );
